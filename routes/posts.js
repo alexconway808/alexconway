9b5351878872900b92d5ddb10f04cb1b2ede84db
Alex Conway <alexconway808@gmail.com>
2014-08-11 19:45:56 -1000

    setup CRUD in seperate posts.js file, filled out

--- routes/posts.js
+++ routes/posts.js
@@ -1,36 +1,7 @@
-//Middleware Dependencies
-var mongoose = require ('./mongoose'); //won't show up in dependencies?
-var methodOverride = require ('method-override'); //need this?
-var session = require ('express-session'); //need this?
-var jade = require ('jade'); 
+
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/user');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
+var router = express.Router();
+var mongoose = require ('../mongoose');
 
 //Blog Tasks
 
@@ -138,46 +109,8 @@ app.delete ('/posts/:id', function (req, res){
     });
 });
 
-module.exports = router;
-
-
-//Authentication
-
 //Comments
 
+//Edit Comments
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
-});
-
-
-module.exports = app;
+module.exports = router;
